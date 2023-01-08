c783b2787c85f6c8273535b59526489315f6a4dc
Johanna Jönsson <johannajonsson96@hotmail.com>
2023-01-09 00:19:05 +0100

    all pokemons page with simple first stage of getting all pokemons (20 at a time) in a list

---
+++ src/pages/PokemonsPage.jsx
@@ -0,0 +1,27 @@
+import Container from 'react-bootstrap/Container'
+import { useQuery } from 'react-query'
+import { getAllPokemons } from '../services/PokeApi'
+
+const PokemonsPage = () => {
+    const {data, isLoading, error, isError} = useQuery(['pokemons'], getAllPokemons)
+
+    return (
+        <Container>
+            <h1>This is a list of all Pokémons</h1>
+
+            {isLoading && (<p>Loading Pokémons...</p>)}
+
+            {isError && (<p>ERROR {error.message}</p>)}
+
+            {data && (
+                <ul>
+                    {data.results.map(i => (
+                        <li key={i.url}>{i.name}</li>
+                    ))}
+                </ul>
+            )}
+        </Container>
+    )
+}
+
+export default PokemonsPage
