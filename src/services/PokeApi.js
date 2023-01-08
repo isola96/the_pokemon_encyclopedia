7f241d3be5dca4df3f409f0af6a25ed5442ce08e
Johanna Jönsson <johannajonsson96@hotmail.com>
2023-01-09 00:18:40 +0100

    get all pokemons from api

---
+++ src/services/PokeApi.js
@@ -0,0 +1,21 @@
+/** 
+ * The RESTful Pokémon API
+ * 
+ * All the Pokémon data you'll ever need in one place, easily accessible through a modern RESTful API.
+ * https://pokeapi.co/docs/v2
+ * 
+ */
+
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://pokeapi.co/api/v2'
+
+const get = async (endpoint) => {
+    const res = await axios.get(endpoint)
+    return res.data
+}
+
+// GET - Get all pokémons 
+export const getAllPokemons = () => {
+    return get(`/pokemon/`)
+}
