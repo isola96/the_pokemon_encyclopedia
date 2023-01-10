dbf965723b32c2f868fedde6d6a3f18152462f94
Johanna Jönsson <johannajonsson96@hotmail.com>
2023-01-10 23:53:13 +0100

    moved pokemons to component

--- src/pages/PokemonsPage.jsx
+++ src/pages/PokemonsPage.jsx
@@ -1,26 +1,11 @@
-import Container from 'react-bootstrap/Container'
-import { useQuery } from 'react-query'
-import { getAllPokemons } from '../services/PokeApi'
+import PokemonsInfo from '../components/PokemonsInfo'
 
 const PokemonsPage = () => {
-    const {data, isLoading, error, isError} = useQuery(['pokemons'], getAllPokemons)
 
     return (
-        <Container>
-            <h1>This is a list of all Pokémons</h1>
-
-            {isLoading && (<p>Loading Pokémons...</p>)}
-
-            {isError && (<p>ERROR {error.message}</p>)}
-
-            {data && (
-                <ul>
-                    {data.results.map(i => (
-                        <li key={i.url}>{i.name}</li>
-                    ))}
-                </ul>
-            )}
-        </Container>
+        <>
+            <PokemonsInfo />
+        </>
     )
 }
 
