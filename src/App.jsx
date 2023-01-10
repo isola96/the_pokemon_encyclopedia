9264d22bca9d227c0d7f1fe66176db825b614481
Johanna Jönsson <johannajonsson96@hotmail.com>
2023-01-10 23:55:15 +0100

    removed path to removed file

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import { Routes, Route } from 'react-router-dom'
 // Pages
 import HomePage from './pages/HomePage'
 import NotFoundPage from './pages/NotFoundPage'
-import PokemonsPage from './pages/PokemonsPage'
 
 function App() {
 	return (
 		<div className="App">
 			<Routes>
 				<Route path="/" element={<HomePage />} />
-				<Route path="/pokemons" element={<PokemonsPage />} />
 				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 		</div>
