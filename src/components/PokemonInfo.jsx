40697c58a422d04f7821d915173f76cddb987b0d
Johanna Jönsson <johannajonsson96@hotmail.com>
2023-01-15 19:19:08 +0100

    added more info

--- src/components/PokemonInfo.jsx
+++ src/components/PokemonInfo.jsx
@@ -7,6 +7,16 @@ const PokemonInfo = ({ goddamnpokemon }) => {
             <Container>
                 <h1>Hello {goddamnpokemon.name}</h1>
                 <img src={goddamnpokemon.sprites.other.dream_world.front_default} alt="pokemon-picture" className='w-50 p-3' />
+                <p>{goddamnpokemon.types[0].type.name}</p>
+
+                {goddamnpokemon.types.length > 1 ? (
+                    <p>{goddamnpokemon.types[1].type.name}</p>
+                ) : (
+                    <p></p>
+                )}
+
+                <p>Height {goddamnpokemon.height}</p>
+                <p>Weight {goddamnpokemon.weight}</p>
             </Container>
         </>
     )
