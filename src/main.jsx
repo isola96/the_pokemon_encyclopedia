0448374d4e99807e5db3a4fda08f41976986c0df
Johanna Jönsson <johannajonsson96@hotmail.com>
2023-01-15 17:39:08 +0100

    added auth context

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App'
+import AuthContextProvider from './contexts/AuthContext'
 
 const queryClient = new QueryClient()
 
@@ -10,7 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
     	<QueryClientProvider client={queryClient}>
 			<BrowserRouter>
-				<App />
+				<AuthContextProvider>
+					<App />
+				</AuthContextProvider>
 			</BrowserRouter>
 		</QueryClientProvider>
   	</React.StrictMode>,
