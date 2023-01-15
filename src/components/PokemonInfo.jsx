d5f72ec1683cb7ee9e1be64a92118aaab9de5b94
Johanna Jönsson <johannajonsson96@hotmail.com>
2023-01-15 17:25:01 +0100

    added picture

--- src/components/PokemonInfo.jsx
+++ src/components/PokemonInfo.jsx
@@ -1,14 +1,15 @@
 import Container from 'react-bootstrap/Container'
 
 const PokemonInfo = ({ goddamnpokemon }) => {
-    console.log(goddamnpokemon)
+    
     return (
         <>
             <Container>
                 <h1>Hello {goddamnpokemon.name}</h1>
+                <img src={goddamnpokemon.sprites.other.dream_world.front_default} alt="pokemon-picture" className='w-50 p-3' />
             </Container>
         </>
     )
 }
 
-export default PokemonInfo
+export default PokemonInfo  
