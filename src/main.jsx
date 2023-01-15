469a1e7f8b5fa2eeb9f4d40f40bb7bf871214d15
Johanna Jönsson <johannajonsson96@hotmail.com>
2023-01-15 19:19:29 +0100

    removed this doesnt work anymore but pushes meanwhile

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App'
-import AuthContextProvider from './contexts/AuthContext'
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -17,9 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
     	<QueryClientProvider client={queryClient}>
 			<BrowserRouter>
-				<AuthContextProvider>
 					<App />
-				</AuthContextProvider>
 			</BrowserRouter>
 		</QueryClientProvider>
   	</React.StrictMode>
