a9fd9fbdcd6bb753e51c5f6644639776ef4d5206
Johanna Jönsson <johannajonsson96@hotmail.com>
2023-01-18 14:15:17 +0100

    fixed error " Function statements require a function name (at firebase_firestore.js... ) "

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    exclude: [
+    'firebase',
+    'firebase/app',
+    'firebase/auth',
+    'firebase/firestore',
+    'firebase/analytics',
+    ],
+    },
   plugins: [react()],
 })
