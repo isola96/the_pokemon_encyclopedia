6a16de84381a2c3b9f8ff6a1f0538fb8de1457cb
Johanna Jönsson <johannajonsson96@hotmail.com>
2023-01-18 15:13:52 +0100

    fixed navbar links to correct if you are logged in or not

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,20 +2,32 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import { Link, NavLink } from 'react-router-dom'
+import { useAuthContext } from '../contexts/AuthContext'
 
 const Navigation = () => {
+	const { currentUser } = useAuthContext()
+
 	return (
-		<Navbar bg="dark" variant="dark" expand="md">
+		<Navbar expand="lg" bg="dark" variant="dark" className="pb-3">
 			<Container>
 				<Navbar.Brand as={Link} to="/">The Pokémon Encyclopedia</Navbar.Brand>
-
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="ms-auto">
-						<Nav.Link as={NavLink} end to="/login">Log In</Nav.Link>
-						<Nav.Link as={NavLink} end to="/logout">Log Out</Nav.Link>
-						<Nav.Link as={NavLink} end to="/signup">Sign Up</Nav.Link>
+								
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="ms-auto align-items-center">
+						{
+							currentUser ? (
+								<>
+									{/* User is logged in */}
+									<Nav.Link as={NavLink} end to="/logout">Log Out</Nav.Link>
+								</>
+							) : (
+								<>
+									{/* No user is logged in */}
+									<Nav.Link as={NavLink} end to="/login">Log In</Nav.Link>
+									<Nav.Link as={NavLink} end to="/signup">Sign Up</Nav.Link>
+								</>
+							)
+						}
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
