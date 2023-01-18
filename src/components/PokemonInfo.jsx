72169b74db9914174db17b5ac34cd23a901894c8
Johanna Jönsson <johannajonsson96@hotmail.com>
2023-01-18 18:39:20 +0100

    removed text

--- src/components/PokemonInfo.jsx
+++ src/components/PokemonInfo.jsx
@@ -5,7 +5,7 @@ const PokemonInfo = ({ goddamnpokemon }) => {
     return (
         <>
             <Container>
-                <h1>Hello {goddamnpokemon.name}</h1>
+                <h1>{goddamnpokemon.name}</h1>
                 <img src={goddamnpokemon.sprites.other.dream_world.front_default} alt="pokemon-picture" className='w-50 p-3' />
                 <p>{goddamnpokemon.types[0].type.name}</p>
 
