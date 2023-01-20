60d4c35e92ae3de57b700f4bdd4026f8659d510d
Johanna Jönsson <johannajonsson96@hotmail.com>
2023-01-20 23:54:43 +0100

    added routing for list page and also auth for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Navigation from './components/Navigation'
 
 // Pages
@@ -10,23 +10,67 @@ import LogOutPage from './pages/LogOutPage'
 import SignUpPage from './pages/SignUpPage'
 import ProfilePage from './pages/ProfilePage'
 import FavouritesPage from './pages/FavouritesPage'
+import ListPage from './pages/ListPage'
+import RequireAuth from './components/RequireAuth'
+import { useAuthContext } from './contexts/AuthContext'
 
 // Styling
+import 'bootstrap/dist/css/bootstrap.css'
 import '../src/assets/App.css'
 
 function App() {
+	const { currentUser } = useAuthContext()
 	return (
 		<div className="App">
 			<Navigation/>
 			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/login" element={<LogInPage />} />
-				<Route path="/logout" element={<LogOutPage />} />
-				<Route path="/signup" element={<SignUpPage />} />
-				<Route path="/profile" element={<ProfilePage />} />
-				<Route path="/favourites" element={<FavouritesPage />} />
-				<Route path="/pokemon/:name" element={<PokemonPage />} />
+				{/*  GUEST */}
+				<Route path="/login" element={
+					currentUser ? <Navigate to={'/'} /> : <LogInPage />
+				} />
+				<Route path="/signup" element={
+					currentUser ? <Navigate to={'/'} /> : <SignUpPage />
+				} />
 				<Route path="*" element={<NotFoundPage />} />
+
+				{/*  LOGGED IN */}
+
+				<Route path="/" element={
+					<RequireAuth>
+						<HomePage />
+					</RequireAuth>
+				} />
+
+				<Route path="/logout" element={
+					<RequireAuth>
+						<LogOutPage />
+					</RequireAuth>
+				} />
+
+				<Route path="/profile" element={
+					<RequireAuth>
+						<ProfilePage />
+					</RequireAuth>
+					
+				} />
+
+				<Route path="/profile/:uid" element={
+					<RequireAuth>
+						<ListPage />
+					</RequireAuth>
+				} />
+
+				<Route path="/favourites" element={
+					<RequireAuth>
+						<FavouritesPage />
+					</RequireAuth>
+				} />
+
+				<Route path="/pokemon/:name" element={
+					<RequireAuth>
+						<PokemonPage />
+					</RequireAuth>
+				} />
 			</Routes>
 		</div>
   	)
