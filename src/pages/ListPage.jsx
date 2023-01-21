b4717008247c18ca70aa62ed8043f02f4702aaeb
Johanna Jönsson <johannajonsson96@hotmail.com>
2023-01-21 18:36:49 +0100

    can delete pokemons from lists

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,19 +1,36 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
 import { useParams } from 'react-router-dom'
 import useStreamCollection from '../hooks/useStreamCollection'
 import { useAuthContext } from '../contexts/AuthContext'
+import { doc, deleteDoc } from 'firebase/firestore'
+import { db } from '../firebase'
+
+
 
 const ListPage = () => {
     const { currentUser } = useAuthContext()
     const { uid } = useParams()
     const {data, loading} = useStreamCollection(`users/${currentUser.uid}/lists/${uid}/pokemons`)
 
+    const handleDelete = async pokemon => {
+        const ref = doc(db, `users/${currentUser.uid}/lists/${uid}/pokemons`, pokemon.id)
+        await deleteDoc(ref)
+    }
+
     return (
         <>
             {data && (
                 <>
                     {data.map((pokemon, index) => (
-                        <div key={index}>{pokemon.name}</div>
+                        <div key={index}>
+                            {pokemon.name}
+                            <Button 
+                                onClick={() => handleDelete(pokemon)} 
+                                className="mt-2"
+                            >Delete
+                            </Button>
+                        </div>
                     ))}
                 </>
             )}
