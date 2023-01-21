7d21b46c87a3c692db98e8ff8ed7f31d1b724602
Johanna Jönsson <johannajonsson96@hotmail.com>
2023-01-21 19:02:15 +0100

    removed not needed code

--- src/components/PokemonInfo.jsx
+++ src/components/PokemonInfo.jsx
@@ -1,8 +1,6 @@
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
-import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
 import React, { useState } from 'react';
 import {collection, addDoc} from 'firebase/firestore'
 import { db } from '../firebase'
