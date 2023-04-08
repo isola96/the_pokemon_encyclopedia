b8f7d9722d9d922c1df6022e6f549b544246b6ef
Johanna Jönsson <johannajonsson96@hotmail.com>
2023-04-08 14:15:47 +0200

    fixed error that made it now work before

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PokemonsInfo from '../components/PokemonsInfo'
 import Search from '../components/Search'
 import { getAllPokemonsLimit } from '../services/PokeApi'
@@ -6,16 +6,20 @@ import { useQuery } from 'react-query'
 
 
 const HomePage = () => {
-    const {data, isLoading, error, isError} = useQuery(['pokemonsSearch'], () => {
+    const {data} = useQuery(['pokemonsSearch'], () => {
         return getAllPokemonsLimit(1279)
     })
 
-    console.log(data.results)
+    console.log(data?.results)
 
     return (
         <>
             <h1>Gotta catch 'em all</h1>
-            <Search details={data.results} /> 
+
+            {data && (
+                <Search details={data?.results} /> 
+            )}
+            
             <PokemonsInfo />
         </>
     )
